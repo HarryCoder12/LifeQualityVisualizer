bd31695f8d74335c587c63fcc390ad508bd3ec16
susrex <susrex@protonmail.com>
2025-02-14 14:52:55 +0100

    Add sports

--- life-quality-visualizer/life_quality_visualizer/__init__.py
+++ life-quality-visualizer/life_quality_visualizer/__init__.py
@@ -1,26 +1,35 @@
 import osmnx as ox
 
-# `True` means retrieve any object with this tag, regardless of value
-# place = ", San Francisco, California"
-# tags = {"building": True}
-# gdf = ox.features.features_from_place(place, tags)
-# gdf.shape
 
-# dist = 1000
-# address = "Prague 13"
-# # features: https://wiki.openstreetmap.org/wiki/Map_features
-tags = {"amenity": ["school"]}
-#
-# schools = ox.features.features_from_address(
-#     address, tags, dist
-# )  # geopandas GeoDataFrame
-#
-# # Export GeoDataFrame to file:
-# schools.to_file("test")
+def get_feature(place, tags):
+    # features: https://wiki.openstreetmap.org/wiki/Map_features
+    return ox.features.features_from_place(place, tags)
 
-# tags = {"amenity": True, "landuse": ["retail", "commercial"], "highway": "bus_stop"}
-gdf = ox.features.features_from_place("Prague, Czechia", tags)
-# gdf.shape
-# print(gdf.to_json())
-with open("exported-data/schools.geojson", "w") as f:
-    f.write(gdf.to_json())
+
+def get_features(place="Prague, Czechia"):
+    features = [
+        {
+            "name": "schools",
+            "tags": {"amenity": ["school"]},
+        },
+        {
+            "name": "sports",
+            "tags": {"building": ["sports_centre", "stadium", "sports_hall"]},
+        },
+    ]
+
+    for feature in features:
+        feature["gdf"] = get_feature(place, feature["tags"])
+    return features
+
+
+def write_geojson(gdf_name):
+    for feature in gdf_name:
+        with open(
+            f"exported-data/{feature["name"]}.geojson", "w", encoding="UTF-8"
+        ) as f:
+            f.write(feature["gdf"].to_json())
+
+
+if __name__ == "__main__":
+    write_geojson(get_features())
